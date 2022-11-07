c196fbadb3bd4d471ac5360d8e49188e3b1442e6
zongzw <a.zong@f5.com>
2022-11-07 16:45:04 +0800

    Add filter parsing functionalities.

--- pkg/parser.go
+++ pkg/parser.go
@@ -18,152 +18,13 @@ func ParseHTTPRoute(hr *gatewayv1beta1.HTTPRoute) (map[string]interface{}, error
 	}
 
 	rlt := map[string]interface{}{}
-
-	// pools from backendRefs
-	for _, rl := range hr.Spec.Rules {
-		for _, br := range rl.BackendRefs {
-			ns := hr.Namespace
-			if br.Namespace != nil {
-				ns = string(*br.Namespace)
-			}
-			name := strings.Join([]string{ns, string(br.Name)}, ".")
-			rlt["ltm/pool/"+name] = map[string]interface{}{
-				"name":    name,
-				"monitor": "min 1 of http tcp",
-				"members": []interface{}{},
-
-				// "minActiveMembers": 0,
-				// TODO: there's at least one field for PATCH. or we may need to fix that
-				// {"code":400,"message":"transaction failed:one or more properties must be specified","errorStack":[],"apiError":2}
-			}
-			svc := ActiveSIGs.GetService(utils.Keyname(ns, string(br.Name)))
-			eps := ActiveSIGs.GetEndpoints(utils.Keyname(ns, string(br.Name)))
-			if svc != nil && eps != nil {
-				if mbs, err := k8s.FormatMembersFromServiceEndpoints(svc, eps); err != nil {
-					return map[string]interface{}{}, err
-				} else {
-					fmtmbs := []interface{}{}
-
-					for _, mb := range mbs {
-						sep := ":"
-						if utils.IsIpv6(mb.IpAddr) {
-							sep = "."
-						}
-						fmtmbs = append(fmtmbs, map[string]interface{}{
-							"name":    fmt.Sprintf("%s%s%d", mb.IpAddr, sep, mb.TargetPort),
-							"address": mb.IpAddr,
-						})
-					}
-					rlt["ltm/pool/"+name].(map[string]interface{})["members"] = fmtmbs
-
-					// TODO: parse ARP resources for flannel type network.
-				}
-			}
-		}
+	if err := parsePoolsFrom(hr, rlt); err != nil {
+		return map[string]interface{}{}, err
 	}
 
-	// irules
-	name := strings.Join([]string{hr.Namespace, hr.Name}, ".")
-	hostnameConditions := []string{}
-	for _, hn := range hr.Spec.Hostnames {
-		hostnameConditions = append(hostnameConditions, fmt.Sprintf(`[HTTP::host] matches "%s"`, hn))
+	if err := parseiRulesFrom(hr, rlt); err != nil {
+		return map[string]interface{}{}, err
 	}
-	hostnameCondition := strings.Join(hostnameConditions, " or ")
-	if hostnameCondition == "" {
-		hostnameCondition = "1 eq 1"
-	}
-
-	rules := []string{}
-	for _, rl := range hr.Spec.Rules {
-		ruleConditions := []string{}
-		for _, match := range rl.Matches {
-			matchConditions := []string{}
-			if match.Path != nil {
-				matchType := gatewayv1beta1.PathMatchPathPrefix
-				if match.Path.Type != nil {
-					matchType = *match.Path.Type
-				}
-				switch matchType {
-				case gatewayv1beta1.PathMatchPathPrefix:
-					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path] starts_with "%s"`, *match.Path.Value))
-				case gatewayv1beta1.PathMatchExact:
-					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path] eq "%s"`, *match.Path.Value))
-				case gatewayv1beta1.PathMatchRegularExpression:
-					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path matches "%s"`, *match.Path.Value))
-				}
-			}
-			if match.Headers != nil {
-				for _, header := range match.Headers {
-					matchType := gatewayv1beta1.HeaderMatchExact
-					if header.Type != nil {
-						matchType = *header.Type
-					}
-					switch matchType {
-					case gatewayv1beta1.HeaderMatchExact:
-						matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::header "%s"] eq "%s"`, header.Name, header.Value))
-					case gatewayv1beta1.HeaderMatchRegularExpression:
-						matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::header "%s"] matches "%s"`, header.Name, header.Value))
-					}
-				}
-			}
-			if match.Method != nil {
-				matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::method] eq "%s"`, *match.Method))
-			}
-			if match.QueryParams != nil {
-				for _, queryParam := range match.QueryParams {
-					matchType := gatewayv1beta1.QueryParamMatchExact
-					if queryParam.Type != nil {
-						matchType = *queryParam.Type
-					}
-					switch matchType {
-					case gatewayv1beta1.QueryParamMatchExact:
-						matchConditions = append(matchConditions, fmt.Sprintf(`[URI::query [HTTP::uri] "%s"] eq "%s"`, queryParam.Name, queryParam.Value))
-					case gatewayv1beta1.QueryParamMatchRegularExpression:
-						matchConditions = append(matchConditions, fmt.Sprintf(`[URI::query [HTTP::uri] "%s"] matches "%s"`, queryParam.Name, queryParam.Value))
-					}
-				}
-			}
-			ruleConditions = append(ruleConditions, strings.Join(matchConditions, " and "))
-		}
-		ruleCondition := strings.Join(ruleConditions, " or ")
-		if ruleCondition == "" {
-			ruleCondition = "1 eq 1"
-		}
-		// TODO: only the last backendRef is used.
-		var pool string
-		for _, br := range rl.BackendRefs {
-			ns := hr.Namespace
-			if br.Namespace != nil {
-				ns = string(*br.Namespace)
-			}
-			pool = strings.Join([]string{ns, string(br.Name)}, ".")
-
-		}
-		rules = append(rules, fmt.Sprintf(`	
-				if { %s } {
-					pool /cis-c-tenant/%s
-				}
-			`, ruleCondition, pool))
-	}
-
-	ruleObj := map[string]interface{}{
-		"name": name,
-		"apiAnonymous": fmt.Sprintf(`
-			when HTTP_REQUEST {
-				log local0. "request host: [HTTP::host], uri: [HTTP::uri], path: [HTTP::path], method: [HTTP::method]"
-				log local0. "headers: [HTTP::header names]"
-				foreach header [HTTP::header names] {
-					log local0. "$header: [HTTP::header value $header]"
-				}
-				log local0. "queryparams: [HTTP::query]"
-				if { %s } {
-					%s
-				}
-			}
-		`, hostnameCondition, strings.Join(rules, "\n")),
-	}
-
-	rlt["ltm/rule/"+name] = ruleObj
 
 	return rlt, nil
 }
@@ -272,4 +133,242 @@ func ParseRelated(gwObjs []*gatewayv1beta1.Gateway, hrObjs []*gatewayv1beta1.HTT
 	return map[string]interface{}{
 		"": rlt,
 	}, nil
+}
+
+func parsePoolsFrom(hr *gatewayv1beta1.HTTPRoute, rlt map[string]interface{}) error {
+	for _, rl := range hr.Spec.Rules {
+		for _, br := range rl.BackendRefs {
+			ns := hr.Namespace
+			if br.Namespace != nil {
+				ns = string(*br.Namespace)
+			}
+			name := strings.Join([]string{ns, string(br.Name)}, ".")
+			rlt["ltm/pool/"+name] = map[string]interface{}{
+				"name":    name,
+				"monitor": "min 1 of http tcp",
+				"members": []interface{}{},
+
+				// "minActiveMembers": 0,
+				// TODO: there's at least one field for PATCH. or we may need to fix that
+				// {"code":400,"message":"transaction failed:one or more properties must be specified","errorStack":[],"apiError":2}
+			}
+			svc := ActiveSIGs.GetService(utils.Keyname(ns, string(br.Name)))
+			eps := ActiveSIGs.GetEndpoints(utils.Keyname(ns, string(br.Name)))
+			if svc != nil && eps != nil {
+				if mbs, err := k8s.FormatMembersFromServiceEndpoints(svc, eps); err != nil {
+					return err
+				} else {
+					fmtmbs := []interface{}{}
+
+					for _, mb := range mbs {
+						sep := ":"
+						if utils.IsIpv6(mb.IpAddr) {
+							sep = "."
+						}
+						fmtmbs = append(fmtmbs, map[string]interface{}{
+							"name":    fmt.Sprintf("%s%s%d", mb.IpAddr, sep, mb.TargetPort),
+							"address": mb.IpAddr,
+						})
+					}
+					rlt["ltm/pool/"+name].(map[string]interface{})["members"] = fmtmbs
+
+					// TODO: parse ARP resources for flannel type network.
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func parseiRulesFrom(hr *gatewayv1beta1.HTTPRoute, rlt map[string]interface{}) error {
+	// irules
+	name := strings.Join([]string{hr.Namespace, hr.Name}, ".")
+	hostnameConditions := []string{}
+	for _, hn := range hr.Spec.Hostnames {
+		hostnameConditions = append(hostnameConditions, fmt.Sprintf(`[HTTP::host] matches "%s"`, hn))
+	}
+	hostnameCondition := strings.Join(hostnameConditions, " or ")
+	if hostnameCondition == "" {
+		hostnameCondition = "1 eq 1"
+	}
+
+	rules := []string{}
+	for _, rl := range hr.Spec.Rules {
+		var pool string = ""
+		ruleConditions := []string{}
+		filterActions := []string{}
+
+		for _, match := range rl.Matches {
+			matchConditions := []string{}
+			if match.Path != nil {
+				matchType := gatewayv1beta1.PathMatchPathPrefix
+				if match.Path.Type != nil {
+					matchType = *match.Path.Type
+				}
+				switch matchType {
+				case gatewayv1beta1.PathMatchPathPrefix:
+					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path] starts_with "%s"`, *match.Path.Value))
+				case gatewayv1beta1.PathMatchExact:
+					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path] eq "%s"`, *match.Path.Value))
+				case gatewayv1beta1.PathMatchRegularExpression:
+					matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::path matches "%s"`, *match.Path.Value))
+				}
+			}
+			if match.Headers != nil {
+				for _, header := range match.Headers {
+					matchType := gatewayv1beta1.HeaderMatchExact
+					if header.Type != nil {
+						matchType = *header.Type
+					}
+					switch matchType {
+					case gatewayv1beta1.HeaderMatchExact:
+						matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::header "%s"] eq "%s"`, header.Name, header.Value))
+					case gatewayv1beta1.HeaderMatchRegularExpression:
+						matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::header "%s"] matches "%s"`, header.Name, header.Value))
+					}
+				}
+			}
+			if match.Method != nil {
+				matchConditions = append(matchConditions, fmt.Sprintf(`[HTTP::method] eq "%s"`, *match.Method))
+			}
+			if match.QueryParams != nil {
+				for _, queryParam := range match.QueryParams {
+					matchType := gatewayv1beta1.QueryParamMatchExact
+					if queryParam.Type != nil {
+						matchType = *queryParam.Type
+					}
+					switch matchType {
+					case gatewayv1beta1.QueryParamMatchExact:
+						matchConditions = append(matchConditions, fmt.Sprintf(`[URI::query [HTTP::uri] "%s"] eq "%s"`, queryParam.Name, queryParam.Value))
+					case gatewayv1beta1.QueryParamMatchRegularExpression:
+						matchConditions = append(matchConditions, fmt.Sprintf(`[URI::query [HTTP::uri] "%s"] matches "%s"`, queryParam.Name, queryParam.Value))
+					}
+				}
+			}
+			ruleConditions = append(ruleConditions, strings.Join(matchConditions, " and "))
+		}
+		ruleCondition := strings.Join(ruleConditions, " or ")
+		if ruleCondition == "" {
+			ruleCondition = "1 eq 1"
+		}
+
+		// filters
+		for _, filter := range rl.Filters {
+			switch filter.Type {
+			case gatewayv1beta1.HTTPRouteFilterRequestHeaderModifier:
+				if filter.RequestHeaderModifier != nil {
+					for _, mdr := range filter.RequestHeaderModifier.Add {
+						filterActions = append(filterActions, fmt.Sprintf("HTTP::header insert %s %s", mdr.Name, mdr.Value))
+					}
+					for _, mdr := range filter.RequestHeaderModifier.Remove {
+						filterActions = append(filterActions, fmt.Sprintf("HTTP::header remove %s", mdr))
+					}
+					for _, mdr := range filter.RequestHeaderModifier.Set {
+						filterActions = append(filterActions, fmt.Sprintf("HTTP::header replace %s %s", mdr.Name, mdr.Value))
+					}
+				}
+			case gatewayv1beta1.HTTPRouteFilterRequestMirror:
+				// filter.RequestMirror.BackendRef -> vs mirror?
+				return fmt.Errorf("filter type '%s' not supported", gatewayv1beta1.HTTPRouteFilterRequestMirror)
+			case gatewayv1beta1.HTTPRouteFilterRequestRedirect:
+				if rr := filter.RequestRedirect; rr != nil {
+					setScheme := `set rscheme "http"`
+					if rr.Scheme != nil {
+						setScheme = fmt.Sprintf(`set rscheme "%s"`, *rr.Scheme)
+					}
+					setHostName := `set rhostname "[HTTP::host]"`
+					if rr.Hostname != nil {
+						setHostName = fmt.Sprintf(`set rhostname "%s"`, *rr.Hostname)
+					}
+
+					// experimental .. definition is not clear yet.
+					setPath := `set rpath "[HTTP::path]"`
+					if rr.Path != nil && rr.Path.ReplaceFullPath != nil {
+						setPath = fmt.Sprintf(`set rpath "%s"`, *rr.Path.ReplaceFullPath)
+					}
+
+					setPort := `set rport [TCP::local_port]`
+					if rr.Port != nil {
+						setPort = fmt.Sprintf(`set rport %d`, *rr.Port)
+					}
+
+					if rr.StatusCode != nil {
+						if *rr.StatusCode != 301 && *rr.StatusCode != 302 {
+							return fmt.Errorf("invalid status %d for request redirect", *rr.StatusCode)
+						}
+					}
+					filterActions = append(filterActions, fmt.Sprintf(`
+						%s
+						%s
+						%s
+						%s
+						set uri $rscheme://$rhostname:$rport$rpath?[HTTP::query]
+						log local0. "request redirect to $uri"
+						HTTP::respond %d Location $uri
+					`, setScheme, setHostName, setPath, setPort, *rr.StatusCode))
+				}
+			// <gateway:experimental>
+			case gatewayv1beta1.HTTPRouteFilterURLRewrite:
+				if ur := filter.URLRewrite; ur != nil {
+					setHostname := `set rhostname [HTTP::host]`
+					if ur.Hostname != nil {
+						setHostname = fmt.Sprintf(`set rhostname "%s"`, *ur.Hostname)
+					}
+					// experimental .. definition is not clear yet.
+					setPath := `set rpath [HTTP::path]`
+					if ur.Path != nil && ur.Path.ReplaceFullPath != nil {
+						setPath = fmt.Sprintf(`set rpath "%s"`, *ur.Path.ReplaceFullPath)
+					}
+					filterActions = append(filterActions, fmt.Sprintf(`
+						%s
+						%s
+						[HTTP::header replace Host $rhostname]
+						HTTP::uri $rpath
+					`, setHostname, setPath))
+				}
+			case gatewayv1beta1.HTTPRouteFilterExtensionRef:
+				if er := filter.ExtensionRef; er != nil {
+					pool := fmt.Sprintf("%s.%s", hr.Namespace, er.Name)
+					filterActions = append(filterActions, fmt.Sprintf("pool /cis-c-tenant/%s", pool))
+				}
+			}
+		}
+		filterAction := strings.Join(filterActions, "\n")
+
+		// TODO: only the last backendRef is used.
+		for _, br := range rl.BackendRefs {
+			ns := hr.Namespace
+			if br.Namespace != nil {
+				ns = string(*br.Namespace)
+			}
+			pn := strings.Join([]string{ns, string(br.Name)}, ".")
+			pool = fmt.Sprintf("pool /cis-c-tenant/%s", pn)
+		}
+		rules = append(rules, fmt.Sprintf(`	
+			if { %s } {
+				%s
+				%s
+			}
+		`, ruleCondition, filterAction, pool))
+	}
+
+	ruleObj := map[string]interface{}{
+		"name": name,
+		"apiAnonymous": fmt.Sprintf(`
+		when HTTP_REQUEST {
+			log local0. "request host: [HTTP::host], uri: [HTTP::uri], path: [HTTP::path], method: [HTTP::method]"
+			log local0. "headers: [HTTP::header names]"
+			foreach header [HTTP::header names] {
+				log local0. "$header: [HTTP::header value $header]"
+			}
+			log local0. "queryparams: [HTTP::query]"
+			if { %s } {
+				%s
+			}
+		}
+	`, hostnameCondition, strings.Join(rules, "\n")),
+	}
+
+	rlt["ltm/rule/"+name] = ruleObj
+	return nil
 }
