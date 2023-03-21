17f41f22f2c77f71887956a078302689bff22594
pzhang <peter.zhang@f5.com>
2023-03-21 14:32:51 +0800

    add webhook function for validate gateway TLS

--- main.go
+++ main.go
@@ -247,30 +247,34 @@ func setupReconcilers(mgr manager.Manager) {
 	}
 }
 
-func setupWebhooks(mgr manager.Manager) error {
+func setupWebhooks(mgr manager.Manager) {
 	slog := utils.NewLog().WithLevel(level).WithRequestID(uuid.NewString())
+
 	if err := (&webhooks.GatewayClassWebhook{Logger: slog}).
 		SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "gatewayclass")
 		os.Exit(1)
 	}
-	if err := (&webhooks.GatewayWebhook{Logger: slog}).
-		SetupWebhookWithManager(mgr); err != nil {
+
+	if err := (&webhooks.GatewayWebhook{
+		Logger: slog,
+		Cache:  mgr.GetCache(),
+	}).SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "gateway")
 		os.Exit(1)
 	}
+
 	if err := (&webhooks.HTTPRouteWebhook{Logger: slog}).
 		SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "httproute")
 		os.Exit(1)
 	}
+
 	if err := (&webhooks.ReferenceGrantWebhook{Logger: slog}).
 		SetupWebhookWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create webhook", "webhook", "referencegrant")
 		os.Exit(1)
 	}
-
-	return nil
 }
 
 func setupBIGIPs(credsDir, confDir string) error {
