31d2c43e6822d2a0e36bd48eb7cb6585fa037f60
zongzw <a.zong@f5.com>
2023-12-22 15:34:55 +0800

    update develop automation scripts for webhook.

--- develop/webhook/setup-webhook-dev.sh
+++ develop/webhook/setup-webhook-dev.sh
@@ -6,15 +6,18 @@ kube_config=/Users/zong/.kube/config
 k="kubectl --kubeconfig $kube_config"
 
 eval "cat <<EOF
-$(< 0.prepare-certificates.yaml.tmpl)
+$(< 1.validating-webhook-configuration.yaml.tmpl)
 EOF
-" > prepare-certificate.yaml
+" > validating-webhook-configuration.yaml
 
-$k apply -f prepare-certificate.yaml
-if [ $? -ne 0 ]; then 
-    echo "Error: Failed to create certificate"
-    exit 1; 
-fi
+$k apply -f validating-webhook-configuration.yaml
+
+eval "cat <<EOF
+$(< 2.vscode-launch.json.tmpl)
+EOF
+" > launch.json
+
+echo "Copy the launch.json to .vscode folder in the project root folder"
 
 while true; do 
     $k get secret/webhook-server-cert -n kube-system; 
@@ -25,18 +28,3 @@ done
 $k get secret webhook-server-cert -n kube-system -o json | jq '.data["tls.crt"]' | tr -d '"' | base64 -d > certificates/tls.crt
 $k get secret webhook-server-cert -n kube-system -o json | jq '.data["tls.key"]' | tr -d '"' | base64 -d > certificates/tls.key
 
-
-eval "cat <<EOF
-$(< 1.validating-webhook-configuration.yaml.tmpl)
-EOF
-" > validating-webhook-configuration.yaml
-
-$k apply -f validating-webhook-configuration.yaml
-
-
-eval "cat <<EOF
-$(< 2.vscode-launch.json.tmpl)
-EOF
-" > launch.json
-
-echo "Copy the launch.json to .vscode folder in the project root folder"
